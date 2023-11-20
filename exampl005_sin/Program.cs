63fa66709b2fee84ca424fe86d493c8e180e6903
Shynar <shurik_aaa@mail.ru>
2023-11-20 14:00:04 +0600

    привет Ш

--- exampl005_sin/Program.cs
+++ exampl005_sin/Program.cs
@@ -1,11 +1,12 @@
 ﻿Console.WriteLine("введите ваше имя");
 string username = Console.ReadLine();
 
-if(username == "Shynar");
+if(username.ToLower() == "Shynar")
 {
-    Console.WriteLine("WWWWooo");
+  Console.WriteLine("WWWWooo");   
 }
-else  
+    //Console.WriteLine("WWWWooo");
+else
 {
     Console.Write ("Привет, ");
     Console.WriteLine(username);
